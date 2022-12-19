193987d3e75188b64de68bbd78bc051b66a269e0
“깃 <hy04730@naver.com>
2022-12-19 23:35:27 +0900

    JSX 문법(1)

--- basic/src/App.js
+++ basic/src/App.js
@@ -2,23 +2,17 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  const name = "jeongyo";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className="orange">Hello1</h1>
+      <h2>Hello2</h2>
+      <p>{name}</p>
+      <ul>
+        <li>딸기</li>
+        <li>우유</li>
+      </ul>
+    </>
   );
 }
 
