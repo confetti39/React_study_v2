147b0c9cb126fd756d2f5b801d994cbef82fc122
“깃 <hy04730@naver.com>
2022-12-19 23:58:08 +0900

    JSX 문법(2)

--- basic/src/App.js
+++ basic/src/App.js
@@ -1,17 +1,23 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   const name = "jeongyo";
+  const list = ["우유", "딸기", "바나나", "요거트"];
   return (
     <>
-      <h1 className="orange">Hello1</h1>
+      <h1 className="orange">Hello {name}</h1>
       <h2>Hello2</h2>
       <p>{name}</p>
       <ul>
-        <li>딸기</li>
-        <li>우유</li>
+        {list.map((item) => (
+          <li>{item}</li>
+        ))}
       </ul>
+      <img
+        style={{ height: "200px" }}
+        src="https://images.unsplash.com/photo-1454923634634-bd1614719a7b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"
+        alt="people"
+      />
     </>
   );
 }
