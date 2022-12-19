26dfa06a297d8485091d8eeba15810c4c5c3badf
“깃 <hy04730@naver.com>
2022-12-20 01:15:51 +0900

    챌린지: new 태그

--- basic/src/components/Profile.jsx
+++ basic/src/components/Profile.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 
-export default function Profile({ image, name, title }) {
+export default function Profile({ image, name, title, isNew }) {
   return (
     <div className="profile">
+      {isNew == "1" ? <p className="new">NEW</p> : null}
       <img className="photo" src={image} alt="avatar" />
       <h1>{name}</h1>
       <p>{title}</p>
