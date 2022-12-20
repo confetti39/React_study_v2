8f5ffffacc00885ab1f7cb97e5e30102e4ec6513
“깃 <hy04730@naver.com>
2022-12-20 21:23:01 +0900

    useState(2)-중요부분

--- basic/src/components/Counter.jsx
+++ basic/src/components/Counter.jsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 export default function Counter() {
   const [count, setCount] = useState(0);
   const handleClick = () => {
-    setCount(count + 1);
+    setCount((prev) => prev + 1);
   };
 
   return (
