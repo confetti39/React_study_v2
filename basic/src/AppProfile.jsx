3cc9dc5adb539952cbcef5f371446ca837fc3165
“깃 <hy04730@naver.com>
2022-12-20 19:31:17 +0900

    챌린지 모범답안: new 태그

--- basic/src/AppProfile.jsx
+++ basic/src/AppProfile.jsx
@@ -8,7 +8,7 @@ function AppProfile() {
         image="https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80"
         name="James Kim"
         title="프론트엔드 개발자"
-        isNew="1"
+        isNew={true}
       />
       <Profile
         image="https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=922&q=80"
--- basic/src/components/Profile.jsx
+++ basic/src/components/Profile.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Profile({ image, name, title, isNew }) {
   return (
     <div className="profile">
-      {isNew == "1" ? <p className="new">NEW</p> : null}
+      {isNew && <span className="new">new</span>}
       <img className="photo" src={image} alt="avatar" />
       <h1>{name}</h1>
       <p>{title}</p>
