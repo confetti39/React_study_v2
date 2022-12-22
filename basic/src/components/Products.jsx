e4c42ec43cd3ecac356325a077458f7937413d91
“깃 <hy04730@naver.com>
2022-12-22 22:54:56 +0900

    useEffect(2)

--- basic/src/components/Products.jsx
+++ basic/src/components/Products.jsx
@@ -1,23 +1,31 @@
 import React, { useEffect, useState } from "react";
 
 export default function Products() {
-  const [count, setCount] = useState(0);
   const [products, setProducts] = useState([]);
+  const [checked, setChecked] = useState(false);
+  const handleChange = () => setChecked((check) => !check);
 
   useEffect(() => {
-    fetch("data/products.json")
+    fetch(`data/${checked ? "sale_" : ""}products.json`)
       .then((res) => res.json())
       .then((data) => {
-        console.log("데이터를 네트워크에서 받아옴");
+        console.log("🔥 데이터를 네트워크에서 받아옴");
         setProducts(data);
       });
     return () => {
-      console.log("깨끗하게 청소하는 일들을 합니다.");
+      console.log("🧹 깨끗하게 청소하는 일들을 합니다.");
     };
-  }, []);
+  }, [checked]);
 
   return (
     <>
+      <input
+        id="checkbox"
+        type="checkbox"
+        value={checked}
+        onChange={handleChange}
+      />
+      <label htmlFor="checkbox">Show Only 🔥 Sale</label>
       <ul>
         {products.map((product) => (
           <li key={product.id}>
@@ -28,7 +36,6 @@ export default function Products() {
           </li>
         ))}
       </ul>
-      <button onClick={() => setCount((prev) => prev + 1)}>{count}</button>
     </>
   );
 }
