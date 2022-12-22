5896a7136fa1bfb4d0c9ecbfe0a0e22c54d87205
“깃 <hy04730@naver.com>
2022-12-22 21:59:52 +0900

    챌린지: total count

--- basic/src/AppCounter.jsx
+++ basic/src/AppCounter.jsx
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import Counter from "./components/Counter";
 import "./App.css";
 
 export default function AppCounter() {
+  const [count, setCount] = useState(0);
+  const handleTotalCount = () => {
+    setCount((prev) => prev + 1);
+  };
   return (
-    <div>
-      <Counter />
+    <div className="container">
+      <div className="banner">
+        Total Count: {count} {count > 10 ? "🔥" : "❄️"}
+      </div>
+      <div className="counters">
+        <Counter handleTotalCount={handleTotalCount} />
+        <Counter handleTotalCount={handleTotalCount} />
+      </div>
     </div>
   );
 }
--- basic/src/components/Counter.jsx
+++ basic/src/components/Counter.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 
-export default function Counter() {
+export default function Counter({ handleTotalCount }) {
   const [count, setCount] = useState(0);
   const handleClick = () => {
     setCount((prev) => prev + 1);
+    handleTotalCount();
   };
 
   return (
