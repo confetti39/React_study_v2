c5db9884d9975cde737ab6e6b2b61dee05e07cda
“깃 <hy04730@naver.com>
2022-12-22 22:35:55 +0900

    useEffect(1)

---
+++ basic/src/AppProducts.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import Products from "./components/Products";
+
+export default function AppProducts() {
+  const [showProducts, setShowProducts] = useState(true);
+  return (
+    <div>
+      {showProducts && <Products />}
+      <button onClick={() => setShowProducts((show) => !show)}>Toggle</button>
+    </div>
+  );
+}
---
+++ basic/src/components/Products.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react";
+
+export default function Products() {
+  const [count, setCount] = useState(0);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("data/products.json")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("데이터를 네트워크에서 받아옴");
+        setProducts(data);
+      });
+    return () => {
+      console.log("깨끗하게 청소하는 일들을 합니다.");
+    };
+  }, []);
+
+  return (
+    <>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            <article>
+              <h3>{product.name}</h3>
+              <p>{product.price}</p>
+            </article>
+          </li>
+        ))}
+      </ul>
+      <button onClick={() => setCount((prev) => prev + 1)}>{count}</button>
+    </>
+  );
+}
