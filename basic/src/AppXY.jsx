106dc823cbae8a5e78f3ec8cdcda40404509e197
“깃 <hy04730@naver.com>
2023-01-10 19:44:02 +0900

    챌린지 모범답안: 마우스 따라가기

--- basic/src/AppXY.jsx
+++ basic/src/AppXY.jsx
@@ -3,18 +3,20 @@ import "./AppXY.css";
 
 export default function AppXY() {
   const [mousePointer, setMousePointer] = useState({ left: 0, top: 0 });
-  useEffect(() => {
-    let pointer = document.querySelector(".pointer");
-    document.addEventListener("mousemove", (event) => {
-      setMousePointer({ left: event.clientX, top: event.clientY });
-      pointer.style.left = mousePointer.left - 15 + "px";
-      pointer.style.top = mousePointer.top - 15 + "px";
-    });
-  }, [mousePointer]);
 
   return (
-    <div className="container">
-      <div className="pointer" />
+    <div
+      className="container"
+      onPointerMove={(e) => {
+        setMousePointer({ left: e.clientX, top: e.clientY });
+      }}
+    >
+      <div
+        className="pointer"
+        style={{
+          transform: `translate(${mousePointer.left}px, ${mousePointer.top}px)`,
+        }}
+      />
     </div>
   );
 }
