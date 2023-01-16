c802810dc9dfefaa153f60e7815a1db8c08109f0
“깃 <hy04730@naver.com>
2023-01-16 20:02:47 +0900

    Immer(1)

--- basic/.pnp.cjs
+++ basic/.pnp.cjs
@@ -32,9 +32,11 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["@testing-library/react", "virtual:a2eb9c761d3f2f5a54785dc48f11530f742a085568a27278c10fafea0fc3c68c6919310052dd90eb9943c50beb3ee043cac359438a44933b56bfcdf611a63aca#npm:13.4.0"],\
             ["@testing-library/user-event", "virtual:a2eb9c761d3f2f5a54785dc48f11530f742a085568a27278c10fafea0fc3c68c6919310052dd90eb9943c50beb3ee043cac359438a44933b56bfcdf611a63aca#npm:13.5.0"],\
             ["eslint-config-react-app", "virtual:a2eb9c761d3f2f5a54785dc48f11530f742a085568a27278c10fafea0fc3c68c6919310052dd90eb9943c50beb3ee043cac359438a44933b56bfcdf611a63aca#npm:7.0.1"],\
+            ["immer", "npm:9.0.18"],\
             ["react", "npm:18.2.0"],\
             ["react-dom", "virtual:a2eb9c761d3f2f5a54785dc48f11530f742a085568a27278c10fafea0fc3c68c6919310052dd90eb9943c50beb3ee043cac359438a44933b56bfcdf611a63aca#npm:18.2.0"],\
             ["react-scripts", "virtual:a2eb9c761d3f2f5a54785dc48f11530f742a085568a27278c10fafea0fc3c68c6919310052dd90eb9943c50beb3ee043cac359438a44933b56bfcdf611a63aca#npm:5.0.1"],\
+            ["use-immer", "virtual:a2eb9c761d3f2f5a54785dc48f11530f742a085568a27278c10fafea0fc3c68c6919310052dd90eb9943c50beb3ee043cac359438a44933b56bfcdf611a63aca#npm:0.8.1"],\
             ["web-vitals", "npm:2.1.4"]\
           ],\
           "linkType": "SOFT"\
@@ -6004,9 +6006,11 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["@testing-library/react", "virtual:a2eb9c761d3f2f5a54785dc48f11530f742a085568a27278c10fafea0fc3c68c6919310052dd90eb9943c50beb3ee043cac359438a44933b56bfcdf611a63aca#npm:13.4.0"],\
             ["@testing-library/user-event", "virtual:a2eb9c761d3f2f5a54785dc48f11530f742a085568a27278c10fafea0fc3c68c6919310052dd90eb9943c50beb3ee043cac359438a44933b56bfcdf611a63aca#npm:13.5.0"],\
             ["eslint-config-react-app", "virtual:a2eb9c761d3f2f5a54785dc48f11530f742a085568a27278c10fafea0fc3c68c6919310052dd90eb9943c50beb3ee043cac359438a44933b56bfcdf611a63aca#npm:7.0.1"],\
+            ["immer", "npm:9.0.18"],\
             ["react", "npm:18.2.0"],\
             ["react-dom", "virtual:a2eb9c761d3f2f5a54785dc48f11530f742a085568a27278c10fafea0fc3c68c6919310052dd90eb9943c50beb3ee043cac359438a44933b56bfcdf611a63aca#npm:18.2.0"],\
             ["react-scripts", "virtual:a2eb9c761d3f2f5a54785dc48f11530f742a085568a27278c10fafea0fc3c68c6919310052dd90eb9943c50beb3ee043cac359438a44933b56bfcdf611a63aca#npm:5.0.1"],\
+            ["use-immer", "virtual:a2eb9c761d3f2f5a54785dc48f11530f742a085568a27278c10fafea0fc3c68c6919310052dd90eb9943c50beb3ee043cac359438a44933b56bfcdf611a63aca#npm:0.8.1"],\
             ["web-vitals", "npm:2.1.4"]\
           ],\
           "linkType": "SOFT"\
@@ -9700,6 +9704,13 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "packageLocation": "./.yarn/cache/immer-npm-9.0.16-426ac657bc-e9a5ca65c9.zip/node_modules/immer/",\
           "packageDependencies": [\
             ["immer", "npm:9.0.16"]\
+          ],\
+          "linkType": "HARD"\
+        }],\
+        ["npm:9.0.18", {\
+          "packageLocation": "./.yarn/cache/immer-npm-9.0.18-c5e105d491-85b3153dd0.zip/node_modules/immer/",\
+          "packageDependencies": [\
+            ["immer", "npm:9.0.18"]\
           ],\
           "linkType": "HARD"\
         }]\
@@ -16482,6 +16493,32 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["url-parse", "npm:1.5.10"],\
             ["querystringify", "npm:2.2.0"],\
             ["requires-port", "npm:1.0.0"]\
+          ],\
+          "linkType": "HARD"\
+        }]\
+      ]],\
+      ["use-immer", [\
+        ["npm:0.8.1", {\
+          "packageLocation": "./.yarn/cache/use-immer-npm-0.8.1-2cf589888f-9ffc1a201a.zip/node_modules/use-immer/",\
+          "packageDependencies": [\
+            ["use-immer", "npm:0.8.1"]\
+          ],\
+          "linkType": "SOFT"\
+        }],\
+        ["virtual:a2eb9c761d3f2f5a54785dc48f11530f742a085568a27278c10fafea0fc3c68c6919310052dd90eb9943c50beb3ee043cac359438a44933b56bfcdf611a63aca#npm:0.8.1", {\
+          "packageLocation": "./.yarn/__virtual__/use-immer-virtual-f6f2f603b3/0/cache/use-immer-npm-0.8.1-2cf589888f-9ffc1a201a.zip/node_modules/use-immer/",\
+          "packageDependencies": [\
+            ["use-immer", "virtual:a2eb9c761d3f2f5a54785dc48f11530f742a085568a27278c10fafea0fc3c68c6919310052dd90eb9943c50beb3ee043cac359438a44933b56bfcdf611a63aca#npm:0.8.1"],\
+            ["@types/immer", null],\
+            ["@types/react", null],\
+            ["immer", "npm:9.0.18"],\
+            ["react", "npm:18.2.0"]\
+          ],\
+          "packagePeers": [\
+            "@types/immer",\
+            "@types/react",\
+            "immer",\
+            "react"\
           ],\
           "linkType": "HARD"\
         }]\
---
+++ basic/src/AppMentorsImmer.jsx
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import { useImmer } from "use-immer";
+
+export default function AppMentorsImmer() {
+  const [person, updatePerson] = useImmer(initialPerson);
+
+  const handleUpdate = () => {
+    const prev = prompt(`누구의 이름을 바꾸고 싶은가요?`);
+    const current = prompt(`이름을 무엇으로 바꾸고 싶은가요?`);
+    updatePerson((person) => {
+      const mentor = person.mentor.map((mentor) => mentor.name === prev);
+      mentor.name = current;
+    });
+  };
+
+  const handleAdd = () => {
+    const name = prompt(`추가하려는 멘토의 이름을 입력하세요.`);
+    const title = prompt(`추가하려는 멘토의 타이틀을 입력하세요.`);
+    updatePerson((person) => person.mentors.push({ name, title }));
+  };
+
+  const handleDelete = () => {
+    const deleteName = prompt(`식제하려는 멘토의 이름을 입력하세요.`);
+    updatePerson((person) => {
+      const index = person.mentors.findIndex(
+        (mentor) => mentor.name === deleteName
+      );
+      person.menotrs.splice(index, 1);
+    });
+  };
+
+  return (
+    <div>
+      <div>
+        <h1>
+          {person.name}는 {person.title}
+        </h1>
+        <p>{person.name}의 멘토는:</p>
+        <ul>
+          {person.mentors.map((mentor) => (
+            <li key={mentor.id}>
+              {mentor.name} ({mentor.title})
+            </li>
+          ))}
+        </ul>
+        <button onClick={handleChangeMentor}>멘토의 이름을 바꾸기</button>
+        <button onClick={handleAddMentor}>멘토 추가하기</button>
+        <button onClick={handleDeleteMentor}>멘토 삭제하기</button>
+      </div>
+    </div>
+  );
+}
+
+const initialPerson = {
+  name: "정요",
+  title: "개발자",
+  mentors: [
+    {
+      id: 1,
+      name: "밥",
+      title: "시니어개발자",
+    },
+    {
+      id: 2,
+      name: "제임스",
+      title: "시니어개발자",
+    },
+  ],
+};
