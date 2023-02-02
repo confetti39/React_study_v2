72b7e08e9be260c552d24cfb7404450d92c193e2
“깃 <hy04730@naver.com>
2023-02-02 20:37:19 +0900

    챌린지 모범답안: 로딩, 에러 상태 추가

--- basic/src/basic/components/Products.jsx
+++ basic/src/basic/components/Products.jsx
@@ -3,53 +3,48 @@ import React, { useEffect, useState } from "react";
 export default function Products() {
   const [products, setProducts] = useState([]);
   const [checked, setChecked] = useState(false);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState();
   const handleChange = () => setChecked((check) => !check);
 
   useEffect(() => {
     setLoading(true);
-    fetch(`data/${checked ? "sale_" : ""}productsd.json`)
+    setError(undefined);
+    fetch(`data/${checked ? "sale_" : ""}products.json`)
       .then((res) => res.json())
       .then((data) => {
         console.log("🔥 데이터를 네트워크에서 받아옴");
         setProducts(data);
         setLoading(false);
       })
-      .catch(() => setError(true))
+      .catch((err) => setError("오류가 발생했음!"))
       .finally(() => setLoading(false));
     return () => {
       console.log("🧹 깨끗하게 청소하는 일들을 합니다.");
     };
   }, [checked]);
 
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>{error}</p>;
   return (
     <>
-      {loading ? (
-        <p>loading...</p>
-      ) : error ? (
-        <p>error...</p>
-      ) : (
-        <>
-          <input
-            id="checkbox"
-            type="checkbox"
-            value={checked}
-            onChange={handleChange}
-          />
-          <label htmlFor="checkbox">Show Only 🔥 Sale</label>
-          <ul>
-            {products.map((product) => (
-              <li key={product.id}>
-                <article>
-                  <h3>{product.name}</h3>
-                  <p>{product.price}</p>
-                </article>
-              </li>
-            ))}
-          </ul>
-        </>
-      )}
+      <input
+        id="checkbox"
+        type="checkbox"
+        value={checked}
+        onChange={handleChange}
+      />
+      <label htmlFor="checkbox">Show Only 🔥 Sale</label>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            <article>
+              <h3>{product.name}</h3>
+              <p>{product.price}</p>
+            </article>
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
