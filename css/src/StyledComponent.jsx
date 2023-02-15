cead6328bb25a7a71b35d72a9938b423a874a1f9
“깃 <hy04730@naver.com>
2023-02-15 19:25:33 +0900

    styled-component(3)-정리

---
+++ css/src/StyledComponent.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import styled, { css } from "styled-components";
+
+const Container = styled.div`
+  display: felx;
+`;
+const Button = styled.button`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid #3c5b69;
+  color: #b9eaff;
+  margin: 0 1em;
+  padding: 0.25em 1em;
+  ${(props) =>
+    props.primary &&
+    css`
+      background: #009cd5;
+      color: white;
+    `};
+`;
+
+export default function StyledComponent() {
+  return (
+    <>
+      <Container>
+        <Button>Normal Button</Button>
+        <Button primary>Primary Button</Button>
+      </Container>
+    </>
+  );
+}
