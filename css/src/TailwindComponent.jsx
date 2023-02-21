bd48ea0a02821ee05672ace728397f9147c9692b
“깃 <hy04730@naver.com>
2023-02-21 22:18:38 +0900

    CI/CD, PR

--- css/src/TailwindComponent.jsx
+++ css/src/TailwindComponent.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function TailwindComponent() {
   return (
     <div>
-      <h1 className="text-8xl">안녕!</h1>
+      <h1 className="text-8xl">안녕!-CI/CD,PR test</h1>
       <button className="bg-blue-500 rounded-xl px-2">Button</button>
     </div>
   );
