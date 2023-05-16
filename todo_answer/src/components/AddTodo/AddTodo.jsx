f03f8deaf51a6cad85388c16298dd54c86e3c241
“깃 <hy04730@naver.com>
2023-05-16 20:23:00 +0900

    챌린지 모범답안: 추가하기 구현

---
+++ todo_answer/src/components/AddTodo/AddTodo.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+
+export default function AddTodo({ onAdd }) {
+  const [text, setText] = useState("");
+  const handleChange = (e) => setText(e.target.value);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (text.trim().length === 0) {
+      return;
+    }
+    onAdd({ id: "고유한값", text, status: "active" });
+    setText("");
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Add Todo"
+        value={text}
+        onChange={handleChange}
+      />
+      <button>Add</button>
+    </form>
+  );
+}
--- todo_answer/src/components/TodoList/TodoList.jsx
+++ todo_answer/src/components/TodoList/TodoList.jsx
@@ -1,17 +1,24 @@
 import React, { useState } from "react";
+import AddTodo from "../AddTodo/AddTodo";
 
 export default function TodoList() {
   const [todos, setTodos] = useState([
     { id: "123", text: "장보기", status: "active" },
-    { id: "123", text: "공부하기", status: "active" },
+    { id: "124", text: "공부하기", status: "active" },
   ]);
+  const handleAdd = (todo) => {
+    setTodos([...todos, todo]);
+  };
   return (
-    <section>
-      <ul>
-        {todos.map((item) => (
-          <li key={item.id}>{item.text}</li>
-        ))}
-      </ul>
-    </section>
+    <div>
+      <section>
+        <ul>
+          {todos.map((item) => (
+            <li key={item.id}>{item.text}</li>
+          ))}
+        </ul>
+      </section>
+      <AddTodo onAdd={handleAdd} />
+    </div>
   );
 }
