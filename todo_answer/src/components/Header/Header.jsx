b8a3efa26f033a9b8f5b552269d745141c4da7e2
“깃 <hy04730@naver.com>
2023-05-22 20:39:27 +0900

    챌린지 모범답안: css(1)

--- todo_answer/src/components/Header/Header.jsx
+++ todo_answer/src/components/Header/Header.jsx
@@ -1,12 +1,16 @@
 import React from "react";
+import styles from "./Header.module.css";
 
 export default function Header({ filters, filter, onFilterChange }) {
   return (
-    <header>
-      <ul>
+    <header className={styles.header}>
+      <ul className={styles.filters}>
         {filters.map((value, index) => (
           <li key={index}>
             <button
+              className={`${styles.filter} ${
+                filter === value && styles.selected
+              }`}
               onClick={() => {
                 onFilterChange(value);
               }}
