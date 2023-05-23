84ba37c2c8e9b22849da5f1ef6b6837d98dbea43
“깃 <hy04730@naver.com>
2023-05-23 22:12:53 +0900

    챌린지 모범답안: 다크모드 셋팅 기억하기

--- todo_answer/src/context/DarkModeContext.jsx
+++ todo_answer/src/context/DarkModeContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 const DarkModeContext = createContext();
 
@@ -8,6 +8,16 @@ export function DarkModeProvider({ children }) {
     setDarkMode(!darkMode);
     updateDarkMode(!darkMode);
   };
+
+  useEffect(() => {
+    const isDark =
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches);
+    setDarkMode(isDark);
+    updateDarkMode(isDark);
+  }, []);
+
   return (
     <DarkModeContext.Provider value={{ darkMode, toggleDarkMode }}>
       {children}
@@ -18,8 +28,10 @@ export function DarkModeProvider({ children }) {
 function updateDarkMode(darkMode) {
   if (darkMode) {
     document.documentElement.classList.add("dark");
+    localStorage.theme = "dark";
   } else {
     document.documentElement.classList.remove("dark");
+    localStorage.theme = "light";
   }
 }
 export const useDarkMode = () => useContext(DarkModeContext);
