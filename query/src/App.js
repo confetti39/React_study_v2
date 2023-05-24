ad998aa8c7df6b85fec76b03b77501f43daf2c11
“깃 <hy04730@naver.com>
2023-05-24 20:17:31 +0900

    react-query 기본 사용

--- query/src/App.js
+++ query/src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
 import "./App.css";
 import MainProducts from "./components/MainProducts";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 export default function App() {
-  return <MainProducts />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <MainProducts />
+    </QueryClientProvider>
+  );
 }
--- query/src/components/Products.jsx
+++ query/src/components/Products.jsx
@@ -1,12 +1,20 @@
 import React, { useState } from "react";
-import useProducts from "../hooks/use-products";
+import { useQuery } from "@tanstack/react-query";
 
 export default function Products() {
   const [checked, setChecked] = useState(false);
-  const [loading, error, products] = useProducts({ salesOnly: checked });
+  const {
+    isLoading,
+    error,
+    data: products,
+  } = useQuery(["products"], async () => {
+    console.log("fetching...");
+    return fetch(`data/products.json`).then((res) => res.json());
+  });
+  // const [loading, error, products] = useProducts({ salesOnly: checked });
   const handleChange = () => setChecked((prev) => !prev);
 
-  if (loading) return <p>Loading...</p>;
+  if (isLoading) return <p>Loading...</p>;
 
   if (error) return <p>{error}</p>;
 
