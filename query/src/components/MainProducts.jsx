d3aa1a39e5249e2ff7d7ec6016f074b6071ac472
“깃 <hy04730@naver.com>
2023-05-25 20:41:47 +0900

    react-query staleTime

--- query/src/components/MainProducts.jsx
+++ query/src/components/MainProducts.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import Products from "./Products";
+import { useQueryClient } from "@tanstack/react-query";
 
 export default function MainProducts() {
   const [showLeftProducts, setShowLeftProducts] = useState(true);
   const [showRightProducts, setShowRightProducts] = useState(true);
+  const client = useQueryClient();
   return (
     <main className="container">
       <div>
@@ -18,6 +20,13 @@ export default function MainProducts() {
           Toggle
         </button>
       </div>
+      <button
+        onClick={() => {
+          client.invalidateQueries(["products", false]);
+        }}
+      >
+        정보가 업데이트 되었음 !
+      </button>
     </main>
   );
 }
